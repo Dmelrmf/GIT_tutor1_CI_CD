12731a824091faca414a418cecdbd3a40b67db7d
DimaRmf <->
2022-11-10 16:03:13 +0300

    test_calcu.py added

---
+++ test_calcu.py
@@ -0,0 +1,21 @@
+from calcu_func import calcu as cal_111
+
+
+def test_calcu(a=str, b=int):
+    c = cal_111(a=str, b=int)
+
+    testt_c = a * b
+
+    assert c == testt_c
+
+
+def test_calcu_two(a=3, b=int):
+    c = cal_111(a=str, b=int)
+
+    test_c = a * b
+
+    assert c != test_c
+
+
+test_calcu('a', 3)
+test_calcu_two(3, 3)
